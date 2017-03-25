834b77070a9b16eb2915cb2cf48dc7e49ac087be
Francesco Malatesta <hellofrancesco@gmail.com>
2017-03-25 19:18:08 +0100

    Add migrations with service provider for the bootstrap load

---
+++ src/Migrations/2017_03_25_142853_create_reactables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReactablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactables', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('reaction_id');
+            $table->integer('reactable_id');
+            $table->string('reactable_type');
+
+            $table->integer('reagent_id');
+            $table->string('reagent_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactables');
+    }
+}
---
+++ src/Providers/ReactionsServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LaravelReactions\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReactionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../Migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
